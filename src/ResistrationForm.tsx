ae7d3b4499796763c5fbf79e472051257782dcf0
Naominm <naomimbugua536@gmail.com>
2025-04-06 17:53:11 +0300

    Add more style to make it more appealing

--- src/ResistrationForm.tsx
+++ src/ResistrationForm.tsx
@@ -1,80 +1,87 @@
-import { Box, Typography, TextField,Paper } from "@mui/material"
+import { Box, Typography, TextField, Paper, Button } from "@mui/material"
 import image from "../src/assets/signup.jpg"
 
 function FormData() {
   return (
     <div>
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
+      <Typography component="h1" variant="h5" mt={3}>
+        Create an Account
+      </Typography>
       <Box sx={{
-        marginTop: 8,
+        marginTop: 6,
         display: 'flex',
         alignItems: 'center',
-        gap:"2rem",
+        justifyContent:'center',
+        gap: "2rem"
       }}>
-     
-      
-        <Paper elevation={5}   sx={{ mt: 1,
-         width: "50%",
-         display:"flex",
-         flexDirection:"column",
-         alignItems:"center",
-         padding:"2rem"
-         
-         }}>
-        <Box sx={{display:"flex", gap:"2rem"}}>
-        <TextField
+
+
+        <Paper elevation={5} sx={{
+          mt: 1,
+          width: "30%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "2rem"
+
+        }}>
+
+          <TextField
             label="First Name"
             name="First Name"
             margin="normal"
+            size="small"
+            fullWidth
             required
           />
-            <TextField
+          <TextField
             label="last Name"
             name="last Name"
             margin="normal"
+            size="small"
+            fullWidth
             required
           />
-        </Box>
-         
-            <TextField
-            label="Email Address"
-            name="Email Address"
-            margin="normal"
-            type="Email"
-            required
-          />
-             <TextField
-            label="Email Address"
-            name="Email Address"
-            margin="normal"
-            type="Email"
-            required
-          />
-             <TextField
-            label="Email Address"
-            name="Email Address"
-            margin="normal"
-            type="Email"
-            required
-          />
-          <button>login</button>
-          </Paper>
-          <Box
-      component="img"
-      src={image}
-      sx={{
-        width:"500px",
-        height:"400px",
-        borderRadius:2,
-        boxShadow:3
-      }
 
-      }
-
-      />
-        </Box>
+          <TextField
+            label="Email Address"
+            name="Email Address"
+            margin="normal"
+            type="email"
+            size="small"
+            fullWidth
+            required
+          />
+          <TextField
+            label="Password"
+            name="Password"
+            margin="normal"
+            type="password"
+            size="small"
+            fullWidth
+            required
+          />
+          <TextField
+            label="Confirm Password"
+            name="Confirm Password"
+            margin="normal"
+            type="password"
+            size="small"
+            fullWidth
+            required
+          />
+          <Button variant="contained" sx={{ mt: "1rem" }}>Register</Button>
+        </Paper>
+        <Box
+          component="img"
+          src={image}
+          sx={{
+            width: "40%",
+            height: "400px",
+            borderRadius: 2,
+            boxShadow: 1
+          }} />
+      </Box>
 
     </div>
   )
