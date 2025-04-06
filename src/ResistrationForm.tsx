25c827667b25d972c601ecbe86682412fef3ba1a
Naominm <naomimbugua536@gmail.com>
2025-04-06 17:35:13 +0300

    Adding a flex and a image for a more visual look

--- src/ResistrationForm.tsx
+++ src/ResistrationForm.tsx
@@ -1,35 +1,81 @@
-import { Box, Typography, TextField } from "@mui/material"
+import { Box, Typography, TextField,Paper } from "@mui/material"
+import image from "../src/assets/signup.jpg"
 
 function FormData() {
   return (
     <div>
-      <Box sx={{
-        marginTop: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        border: "2px solid red"
-      }}>
         <Typography component="h1" variant="h5">
           Sign Up
         </Typography>
-        <Box component="form" sx={{ mt: 1, width: "100%" }}>
-          <TextField
+      <Box sx={{
+        marginTop: 8,
+        display: 'flex',
+        alignItems: 'center',
+        gap:"2rem",
+      }}>
+     
+      
+        <Paper elevation={5}   sx={{ mt: 1,
+         width: "50%",
+         display:"flex",
+         flexDirection:"column",
+         alignItems:"center",
+         padding:"2rem"
+         
+         }}>
+        <Box sx={{display:"flex", gap:"2rem"}}>
+        <TextField
             label="First Name"
             name="First Name"
-            fullWidth
             margin="normal"
             required
           />
             <TextField
             label="last Name"
             name="last Name"
-            fullWidth
             margin="normal"
             required
           />
         </Box>
-      </Box>
+         
+            <TextField
+            label="Email Address"
+            name="Email Address"
+            margin="normal"
+            type="Email"
+            required
+          />
+             <TextField
+            label="Email Address"
+            name="Email Address"
+            margin="normal"
+            type="Email"
+            required
+          />
+             <TextField
+            label="Email Address"
+            name="Email Address"
+            margin="normal"
+            type="Email"
+            required
+          />
+          <button>login</button>
+          </Paper>
+          <Box
+      component="img"
+      src={image}
+      sx={{
+        width:"500px",
+        height:"400px",
+        borderRadius:2,
+        boxShadow:3
+      }
+
+      }
+
+      />
+        </Box>
+
     </div>
   )
 }
