4544a0094683d5ed8a24b0d7efa5ab5e757d956a
Naominm <naomimbugua536@gmail.com>
2025-04-06 16:52:11 +0300

    Add First Input field

--- src/ResistrationForm.tsx
+++ src/ResistrationForm.tsx
@@ -1,14 +1,30 @@
-import {Box,Typography } from "@mui/material"
+import { Box, Typography, TextField } from "@mui/material"
 
-
-function FormData(){
-    return(
-   <div>
-       <Box/>
-      <Typography component="h1" variant="h5">
-        Sign Up </Typography>
-   </div>
-    )
+function FormData() {
+  return (
+    <div>
+      <Box sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        border: "2px solid red"
+      }}>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form" sx={{ mt: 1, width: "100%" }}>
+          <TextField
+            label="First Name"
+            name="First Name"
+            fullWidth
+            margin="normal"
+            required
+          />
+        </Box>
+      </Box>
+    </div>
+  )
 }
 
 export default FormData;
